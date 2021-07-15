be3b75a30fa69fff13e81332c3a67939ecc7a31b
TNaingHtun <scm.tinnainghtun@gmail.com>
2021-07-15 11:41:45 +0630

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CreateProfileSeeder::class);
+        $this->call(CreateUsersSeeder::class);
     }
 }
